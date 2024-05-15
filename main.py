dc66de163e2e7b64b9d01be45502526e6d815d61
gerrich <nplitlyceum@gmail.com>
2024-05-15 09:23:54 +0300

    some

--- main.py
+++ main.py
@@ -1,52 +1,36 @@
-import os
 import subprocess
 import sys
 import json
 import time
 
-import requests
-
-# services = {
-#   'name': '',
-#   'github_token': '',
-#   'repository_name': '',
-#   'repository_folder': ''
-# }
+# services = [{
+#   'service_name': '',
+#   'repository_folder': '',
+#   'local_branch_name': 'master',
+#   'remote_branch_name': 'origin/master'
+# },]
 
 
 # Функция для обновления кода из GitHub
 def update_code(service):
     # Остановка сервиса
-    subprocess.run(['systemctl', 'stop', service['name']])
-
-    # Получаем список коммитов из GitHub
-    response = requests.get(f'https://api.github.com/repos/{service["repository_name"]}/commits',
-                            headers={'Authorization': f'Bearer {service["github_token"]}'})
-    commits = response.json()
-
-    # Получаем последний коммит
-    last_commit = commits[0]
-
-    # Клонируем репозиторий, если он не существует
-    if not os.path.exists(service["repository_folder"]):
-        subprocess.run(['git', 'clone',
-                        f'https://github.com/{service["repository_name"]}.git', service["repository_folder"]])
+    subprocess.run(['systemctl', 'stop', service['service_name']])
 
     # Обновляем код из репозитория
     subprocess.run(['git', 'pull'], cwd=service["repository_folder"])
 
     # Перезапуск сервиса
-    subprocess.run(['systemctl', 'start', service['name']])
+    subprocess.run(['systemctl', 'start', service['service_name']])
 
 
 # Слушаем события из GitHub
 def listen_events(service):
-    response = requests.get(f'https://api.github.com/repos/{service["repository_name"]}/events',
-                            headers={'Authorization': f'Bearer {service["github_token"]}'})
-    events = response.json()
+    cmd = ["git", "rev-list", "--count", "--left-right", "origin/master..master"]
+    result = subprocess.run(cmd, cwd=service["repository_folder"], capture_output=True, text=True)
 
-    for event in events:
-        handle_event(event['type'], service)
+    output = result.stdout.strip()
+    if output != "0":
+        update_code(service)
 
 
 def main(services_file_path):
